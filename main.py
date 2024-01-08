6ed16f80491e8d82092a265ee31d8c67d7410a65
Zhmot666 <mailboxforspam@tut.by>
2024-01-08 12:48:45 +0300

    Черновой вариант готов. Можно пользоваться, но программа требует универсализации, что бы работала независимо от количества уровней JSON

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import json
+import copy
 import xlsxwriter
 import tkinter as tk
 from tkinter import filedialog
 
 
-def read_lev(lev_up, bc_list):
-    if type(lev_up) == list:
+def write_bc(lev_dic, level, wr_barcode):
+    lev_dic['lv'+str(level)] = wr_barcode
+    return lev_dic
+
+
+def read_lev(lev_up, bc_list, lev_dic):
+    if isinstance(lev_up, list):
         for lev_key in lev_up:
-            read_lev(lev_key, bc_list)
+            bc_list = read_lev(lev_key, bc_list, lev_dic)
         return bc_list
-    elif type(lev_up) == dict:
+    elif isinstance(lev_up, dict):
         if 'ChildBarcodes' in lev_up:
-            read_lev(lev_up['ChildBarcodes'], bc_list)
+            if 'level' in lev_up:
+                lev_dic = write_bc(lev_dic, lev_up['level'], lev_up['Barcode'])
+            bc_list = read_lev(lev_up['ChildBarcodes'], bc_list, lev_dic)
         elif 'Barcodes' in lev_up:
-            read_lev(lev_up['Barcodes'], bc_list)
+            bc_list = read_lev(lev_up['Barcodes'], bc_list, lev_dic)
         elif 'Barcode' in lev_up:
-            bc_list.append(lev_up['Barcode'])
+            if 'level' in lev_up:
+                lev_dic = write_bc(lev_dic, lev_up['level'], lev_up['Barcode'])
+            if variant == 1:
+                bc_list.append(lev_up['Barcode'])
+            else:
+                copy_lev_dic = copy.copy(lev_dic)
+                bc_list.append(copy_lev_dic)
         return bc_list
 
 
@@ -24,16 +38,27 @@ if __name__ == "__main__":
     root.withdraw()
     file_path = filedialog.askopenfilename()
 
+    variant = 2  # Вариант 1 - список, вариант 2 - словарь
+
     barcode_list = list()
-    with open(file_path) as read_file:
+    with open(file_path, encoding='utf-8') as read_file:
         templates = json.load(read_file)
         lev1 = templates["TaskMarks"]
-        read_lev(lev1, barcode_list)
+        level_dic = dict()
+        level_dic['lv2'] = ''
+        level_dic['lv1'] = ''
+        level_dic['lv0'] = ''
+        read_lev(lev1, barcode_list, level_dic)
 
     counter = 1
-    workbook = xlsxwriter.Workbook('gematogen.xlsx')
+    workbook = xlsxwriter.Workbook(file_path.replace('json', 'xlsx'))
     worksheet = workbook.add_worksheet()
     for barcode in barcode_list:
-        worksheet.write('A'+str(counter), barcode)
+        if variant == 1:
+            worksheet.write('A' + str(counter), barcode)
+        else:
+            worksheet.write('A' + str(counter), barcode['lv0'])
+            worksheet.write('B' + str(counter), barcode['lv1'])
+            worksheet.write('C' + str(counter), barcode['lv2'])
         counter += 1
     workbook.close()
