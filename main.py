6d27cff18f8cf1724af3f66b05f1ef1e18548646
Zhmot666 <mailboxforspam@tut.by>
2024-01-08 16:53:55 +0300

    Начал переделывать в универсал

--- main.py
+++ main.py
@@ -25,40 +25,35 @@ def read_lev(lev_up, bc_list, lev_dic):
         elif 'Barcode' in lev_up:
             if 'level' in lev_up:
                 lev_dic = write_bc(lev_dic, lev_up['level'], lev_up['Barcode'])
-            if variant == 1:
-                bc_list.append(lev_up['Barcode'])
-            else:
-                copy_lev_dic = copy.copy(lev_dic)
-                bc_list.append(copy_lev_dic)
+            copy_lev_dic = copy.copy(lev_dic)
+            bc_list.append(copy_lev_dic)
         return bc_list
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.withdraw()
-    file_path = filedialog.askopenfilename()
-
-    variant = 2  # Вариант 1 - список, вариант 2 - словарь
+    file_path = filedialog.askopenfilename(title='Выберите JSON-файл для конвертирования', defaultextension='*.json',
+                                           filetypes=[('JSON файлы', '*.json'), ('All files', '*.*')])
 
     barcode_list = list()
+    level_dic = dict()
+    with open(file_path, encoding='utf-8') as read_file:
+        for line in read_file:
+            if 'level' in line:
+                num_level = (line.replace('"level": ', '')).strip()
+                if 'lv' + num_level not in level_dic:
+                    level_dic['lv' + num_level] = ''
     with open(file_path, encoding='utf-8') as read_file:
         templates = json.load(read_file)
         lev1 = templates["TaskMarks"]
-        level_dic = dict()
-        level_dic['lv2'] = ''
-        level_dic['lv1'] = ''
-        level_dic['lv0'] = ''
         read_lev(lev1, barcode_list, level_dic)
 
-    counter = 1
     workbook = xlsxwriter.Workbook(file_path.replace('json', 'xlsx'))
     worksheet = workbook.add_worksheet()
     for barcode in barcode_list:
-        if variant == 1:
-            worksheet.write('A' + str(counter), barcode)
-        else:
-            worksheet.write('A' + str(counter), barcode['lv0'])
-            worksheet.write('B' + str(counter), barcode['lv1'])
-            worksheet.write('C' + str(counter), barcode['lv2'])
+        counter = 1
+        for i in range(int(num_level) + 1):
+            worksheet.write(chr(65+i) + str(counter), barcode['lv'+str(i)])
         counter += 1
     workbook.close()
